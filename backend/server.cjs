3404e60d5588c6da39d0c720bcf7e46a0c414df9
SORNCHAI PHODEE <6231305021@lamduan.mfu.ac.th>
2025-09-05 00:52:51 +0700

    Ready for Railway deploy

--- backend/server.cjs
+++ backend/server.cjs
@@ -15,12 +15,15 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+ port: process.env.DB_PORT || 3306 // ต้องใส่
 });
 
 db.connect((err) => {
-  if (err) console.error("❌ MySQL connection error:", err);
-  else console.log("✅ Connected to MySQL!");
+  if (err) {
+    console.error("❌ MySQL connection error:", err);
+  } else {
+    console.log("✅ Connected to MySQL!");
+  }
 });
 
 // Serve frontend dist
