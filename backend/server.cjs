291d8162e1062c40a2232a753d10fb1562fc531d
SORNCHAI PHODEE <6231305021@lamduan.mfu.ac.th>
2025-09-23 01:19:56 +0700

    edsv

--- backend/server.cjs
+++ backend/server.cjs
@@ -12,11 +12,11 @@ app.use(express.json());
 
 // -------------------- MySQL --------------------
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT) || 3306, // MySQL port
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: parseInt(process.env.MYSQLPORT) || 3306, // MySQL port
 });
 
 db.connect((err) => {
